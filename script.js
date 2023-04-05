95c555ff0be89cd55bdd1eaf33264a498ada7316
igorsctt <igor.ss2020@hotmail.com>
2023-04-05 08:29:15 -0300

    Meu Portfolio

---
+++ script.js
@@ -0,0 +1,28 @@
+function escrevendoLetras(){
+    function ativaLetra(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((letra, i)=>{
+        setTimeout(()=>{
+            elemento.innerHTML += letra
+        }, 75 * i)
+    })
+}
+
+const titulo = document.querySelector('.digitando');
+
+ativaLetra(titulo);
+}
+
+escrevendoLetras();
+
+
+const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria');
+
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x');
+    navMenu.classList.toggle('ativado')
+})
+
